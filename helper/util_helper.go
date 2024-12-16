93da0c6b0f8d454d879f82de613b0d79c04987c9
hafizhzikry24 <hafizhcool24@gmail.com>
2024-12-16 13:12:23 +0700

    first-commit

---
+++ helper/util_helper.go
@@ -0,0 +1,17 @@
+package helper
+
+func PanicIfError(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func MessageForTag(tag string) string {
+	switch tag {
+
+	case "required":
+		return "this field is required"
+	}
+
+	return ""
+}
